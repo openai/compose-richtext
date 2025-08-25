6f8fbea2bda801b1a9d2c888239e4d5883c3e51e
RJ Marsan <rj@openai.com>
2025-08-25 10:02:06 -0700

    Nice nic enice

--- richtext-ui/src/commonMain/kotlin/com/halilibo/richtext/ui/MarkdownAnimation.kt
+++ richtext-ui/src/commonMain/kotlin/com/halilibo/richtext/ui/MarkdownAnimation.kt
@@ -1,46 +1,42 @@
 package com.halilibo.richtext.ui
 
-import androidx.compose.animation.core.animate
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import com.halilibo.richtext.ui.string.MarkdownAnimationState
 import com.halilibo.richtext.ui.string.RichTextRenderOptions
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.milliseconds
 
 @Composable
 internal fun rememberMarkdownFade(
   richTextRenderOptions: RichTextRenderOptions,
   markdownAnimationState: MarkdownAnimationState,
-  enqueue: Boolean = true,
 ): State<Float> {
   val coroutineScope = rememberCoroutineScope()
-  val alpha = remember { mutableFloatStateOf(if (richTextRenderOptions.animate) 0f else 1f) }
+  val targetAlpha = remember {
+    mutableFloatStateOf(if (richTextRenderOptions.animate) 0f else 1f)
+  }
   LaunchedEffect(Unit) {
-    if (enqueue) {
+    if (richTextRenderOptions.animate) {
       coroutineScope.launch {
         markdownAnimationState.addAnimation(richTextRenderOptions)
-        animate(
-          initialValue = 0f,
-          targetValue = 1f,
-          animationSpec = tween(
-            durationMillis = richTextRenderOptions.textFadeInMs,
-            delayMillis = markdownAnimationState.toDelayMs(),
-          )
-        ) { value, _ ->
-          alpha.floatValue = value
-        }
+        delay(markdownAnimationState.toDelayMs().milliseconds)
+        targetAlpha.floatValue = 1f
       }
-    } else {
-      alpha.floatValue = 1f
     }
   }
+  val alpha = animateFloatAsState(
+    targetAlpha.floatValue,
+    tween(
+      durationMillis = richTextRenderOptions.textFadeInMs,
+    )
+  )
   return alpha
 }
